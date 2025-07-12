e425b9728e5d668a5b9d382705267cbcd9475b5b
Keyur-2001 <keyur.it2001@gmail.com>
2025-07-12 12:31:12 +0530

    updated code for bug solve

--- routes/DashboardCountsRoutes.js
+++ routes/DashboardCountsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const DashboardCountsController = require('../controllers/dashboardCountsController');
+const DashboardCountsController = require('../controllers/DashboardCountsController');
 
 router.get('/', DashboardCountsController.getDashboardCounts);
 
